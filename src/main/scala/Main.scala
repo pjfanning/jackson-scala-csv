4fcb26e43ffcb49c7185e4e2b67cc6f5c15cf1f1
PJ Fanning <pjfanning@users.noreply.github.com>
2021-08-21 12:39:56 +0100

    different samples

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,24 +3,26 @@ package com.github.pjfanning.jackson.csv
 import com.fasterxml.jackson.core.`type`.TypeReference
 import com.fasterxml.jackson.databind.MappingIterator
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import com.fasterxml.jackson.dataformat.csv.{CsvMapper, CsvParser}
+import com.fasterxml.jackson.dataformat.csv.{CsvMapper, CsvParser, CsvSchema}
 
-import scala.jdk.CollectionConverters._
+import scala.jdk.CollectionConverters.*
 
-case class OrderLine(item: String, quantity: Int, unitPrice: BigDecimal)
+case class OrderLine(item: String, quantity: Int, unitPrice: Option[BigDecimal])
 
 object Main extends App {
   val csvMapper = CsvMapper.builder()
     .addModule(DefaultScalaModule)
     .enable(CsvParser.Feature.TRIM_SPACES)
     .build()
-  val orderLineSchema = csvMapper.schemaFor(classOf[OrderLine]).withHeader
-  println(s"schema: ${orderLineSchema.getColumnDesc}")
-  val orderLinesIterator: MappingIterator[OrderLine] = csvMapper.readerFor(classOf[OrderLine]).`with`(orderLineSchema)
-    .readValues(FileUtil.readBytes("orderLines.csv"))
+
+  val readerSchema = CsvSchema.builder().setUseHeader(true).setReorderColumns(true).build()
+  val orderLinesIterator: MappingIterator[OrderLine] = csvMapper.readerFor(classOf[OrderLine]).`with`(readerSchema)
+    .readValues(FileUtil.readBytes("orderLines-wrong-header-order.csv"))
   val orderLines = orderLinesIterator.readAll().asScala.toSeq
   println(s"read $orderLines")
 
+  val orderLineSchema = csvMapper.schemaFor(classOf[OrderLine]).withHeader
+  println(s"schema: ${orderLineSchema.getColumnDesc}")
   val outputCsv = csvMapper.writerFor(new TypeReference[Seq[OrderLine]]{}).`with`(orderLineSchema).writeValueAsString(orderLines)
   println(s"wrote:\n$outputCsv")
 }
